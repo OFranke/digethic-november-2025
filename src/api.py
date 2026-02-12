0478526769401b62d953a2b68ab116c81d3d6fb8
Oliver Franke <oliver.franke@limebit.de>
2026-02-12 13:58:45 +0100

    fix cors

--- src/api.py
+++ src/api.py
@@ -1,9 +1,11 @@
 from flask import Flask, Response, request
+from flask_cors import CORS
 import pandas as pd
 import os
 import pickle
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/", methods=["GET"])
